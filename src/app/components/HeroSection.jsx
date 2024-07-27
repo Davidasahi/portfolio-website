443a1a412bbffbb28b93f184a62fc93503277e6e
Davidasahi <maple11303@gmail.com>
2024-07-27 16:55:25 -0500

    Set up the page.js

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const HeroSection = () => {
+  return (
+    <section>
+        
+      <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+        Hi I am David
+      </h1>
+      <p className="text-[#ADB7BE] text-lg lg:text-xl">
+        How to contact us for tes for technicas for technicachnical support
+      </p>
+    </section>
+  );
+};
