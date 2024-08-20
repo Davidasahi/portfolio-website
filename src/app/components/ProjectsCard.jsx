5cdc74b905a1fc3f72f72565e0046d9b636f4ea5
Davidasahi <maple11303@gmail.com>
2024-08-19 23:53:14 -0400

    finished Link and CodeBreaketIcon on ProjectsCard

--- src/app/components/ProjectsCard.jsx
+++ src/app/components/ProjectsCard.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 const ProjectsCard = ({ imgUrl, title, description }) => {
   return (
     <div>
       <div
-        className="h-52 md:h-72 rounded-t-xl "
+        className="h-52 md:h-72 rounded-t-xl ralative group"
         style={{ background: `url(${imgUrl})`, backgroundSize: 'cover' }}
-      ></div>
+      >
+        <div className="overlay obselute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transation-all duration-500">
+          <Link
+            href="/"
+            className="h-13 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white"
+          >
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] m-2 cursor-pointer hover:white" />
+          </Link>
+        </div>
+      </div>
       <div className="text-white rounded-b-xl bg-[#181818]py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
