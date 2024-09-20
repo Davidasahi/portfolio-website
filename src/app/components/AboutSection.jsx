991b680d8f711b816bf412849f0f4427f0f02a5c
Davidasahi <maple11303@gmail.com>
2024-09-19 20:58:39 -0500

    Fixed Picture

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -56,7 +56,7 @@ const AboutSection = () => {
   return (
     <section id="about" className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/eagle-head-beak.jpg" width={500} height={500} />
+        <Image alt="aboutMePicture" src="/images/eagle-head-beak.jpg" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold mb-4">About Me</h2>
           <p className="text-base md:text-lg">
