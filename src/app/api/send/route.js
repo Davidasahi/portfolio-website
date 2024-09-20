980383edc4334d8a41ff6355c99d3271ef23dfdc
Davidasahi <maple11303@gmail.com>
2024-09-19 23:32:10 -0500

    Email fixed

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,36 +1,37 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import { Resend } from 'resend';
+import { NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { body } = await req.json();
-  const { email, subject, message } = body;
-  console.log(body);
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
 
   try {
     const { data, error } = await resend.emails.send({
       from: fromEmail,
-      to: ['maple11303@gmail.com', email],
+      to: [email],
       subject: subject,
       react: (
         <>
-          <h1>{subject}</h1>
+          <h3>Hi!</h3>
           <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
           <p>{message}</p>
         </>
       ),
       // EmailTemplate({ firstName: 'John', message: message }),
     });
-
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return res.json({ error }, { status: 500 });
     }
-
-    return Response.json(data);
+    // return res.status(200).json({ data: 'Email sent successfully' });
+    return NextResponse.status(200).json({
+      data: 'Email sent successfully',
+      data,
+    });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -113,7 +113,7 @@ const EmailSection = () => {
           </div>
           <button
             type="submit"
-            className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+            className="bg-primary-500 hover:bg-primary-700 text-white font-medium py-2.5 px-5 rounded-lg w-full"
           >
             Send Message
           </button>
