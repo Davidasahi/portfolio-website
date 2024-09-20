5eafd2337c6c3a699e7bf2408b5b2e493741d155
Davidasahi <maple11303@gmail.com>
2024-09-19 23:44:26 -0500

    Fixed Email problem!

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -5,11 +5,11 @@ import { NextResponse } from 'next/server';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
-
+export async function POST(req) {
   try {
+    const { email, subject, message } = await req.json();
+    console.log(email, subject, message);
+
     const { data, error } = await resend.emails.send({
       from: fromEmail,
       to: [email],
@@ -18,20 +18,28 @@ export async function POST(req, res) {
         <>
           <h3>Hi!</h3>
           <p>Thank you for contacting us!</p>
+          <p>Your message:</p>
           <p>{message}</p>
+          <p>After reviewing your message, I will email you back soon!</p>
         </>
       ),
       // EmailTemplate({ firstName: 'John', message: message }),
     });
     if (error) {
-      return res.json({ error }, { status: 500 });
+      // Return error status and message if the send operation fails
+      return NextResponse.json({ error: error.message }, { status: 500 });
     }
-    // return res.status(200).json({ data: 'Email sent successfully' });
-    return NextResponse.status(200).json({
-      data: 'Email sent successfully',
-      data,
-    });
+    // Send a success response
+    return NextResponse.json(
+      { data: 'Email sent successfully', data },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    console.error('Error in email sending:', error);
+    // Handle any unexpected errors
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
 }
